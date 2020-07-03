dd79fddd2d54313fd9d7628ec53e90b245dc86f3
ayoakpata <akpata.ayobami@gmail.com>
2020-07-03 20:50:48 +0100

    reportingForDuty function

---
+++ day16.js
@@ -0,0 +1,12 @@
+/* Write a function, reportingForDuty(), 
+that has two string parameters, 
+rank and lastName, and returns a string in the following format: 
+‘rank lastName reporting for duty!’ */
+
+const reportingForDuty = (rank, lastName) =>
+  `${rank} ${lastName} reporting for duty!`;
+
+// As a function declaration:
+function reportingForDuty(rank, lastName) {
+  return `${rank} ${lastName} reporting for duty!`;
+}
