4cea52faa42d355ee232ff3937b018387df92e93
ayoakpata <akpata.ayobami@gmail.com>
2020-07-04 22:21:16 +0100

    a truthyOrFalsy(), function

---
+++ day19.js
@@ -0,0 +1,17 @@
+/* a function, truthyOrFalsy(), 
+that takes in any value and returns true 
+if that value is truthy and false if that value is falsy.
+*/
+
+function truthyOrFalsy(value) {
+  if (value) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// Uncomment the line below when you're ready to try out your function
+console.log(truthyOrFalsy(0)); // Should print false
+
+// We encourage you to add more function calls of your own to test your code!
