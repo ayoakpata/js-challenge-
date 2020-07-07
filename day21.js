91decb3f9919dade188ac6e95c10aa6ee8e1f6d3
ayoakpata <akpata.ayobami@gmail.com>
2020-07-07 22:17:15 +0100

    whatRelation() function

---
+++ day21.js
@@ -0,0 +1,17 @@
+/*a function, sillySentence(), that has 3 string parameters and 
+returns the following silly sentence with the blanks filled in 
+by the arguments passed into the function: */
+
+//sillySentence('excited', 'love', 'functions');
+/* Should return 'I am so excited because I love coding! 
+Time to write some more awesome functions!'*/
+
+const sillySentence = (excited, love, functions) => {
+  return `I am so ${excited} because I ${love} coding! 
+    Time to write some more awesome ${functions}!`;
+};
+
+// Uncomment the line below when you're ready to try out your function
+//console.log(sillySentence('excited', 'love', 'functions'))
+
+//add more function calls of your own to test your code!
